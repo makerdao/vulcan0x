c66d1c3537ba004d5a82b02c2cb26239189acb73
dizzy <george.niculae79@gmail.com>
2018-06-03 20:55:48 +0300

    Fix Dockerhub login

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push makerdao/vulcan0x
